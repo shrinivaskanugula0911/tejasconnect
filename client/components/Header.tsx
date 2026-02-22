447a7ae1c107cab056e906021080aed725e8ac04
shrinivaskanugula0911 <shrinivaskanugula07@gmail.com>
2026-02-22 22:05:20 +0530

    Header Logo

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-3 hover:opacity-90 transition-opacity duration-150">
           <img
-            src="https://cdn.builder.io/api/v1/image/assets%2F57d9ad027eda4a5381433d555f15e3ef%2F85423baee08f4b96b5be72c0fbd1d7d5"
+            src="/tejas-connect-logo.png"
             alt="Tejas Connect Logo"
             className="h-10 object-contain"
             style={{ width: '30px' }}
